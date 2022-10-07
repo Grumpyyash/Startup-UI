370beda5b8ebd321b5c7d323ad4d1c0b62e5fe6e
theneek14 <neel.19je0558@mc.iitism.ac.in>
2022-10-08 01:14:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function tododark(){
   document.body.classList.toggle("dark-mode");
-  var cc=document.querySelector('.container');
+  var cc=document.querySelector('.container-fluid');
   cc.classList.toggle("container-color");
 };
