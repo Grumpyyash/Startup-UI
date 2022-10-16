1fec0ff4758a3f1b3fcc10d5ab3437a750dca235
= <=>
2022-10-16 14:16:47 +0530

    change some css styles

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function random_bg_color(){
   var y = Math.floor(Math.random() * 256);
   var z = Math.floor(Math.random() * 256);
   var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- console.log(bgColor);
+  console.log(bgColor);
   document.body.style.background=bgColor;
 }
